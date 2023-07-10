ffd3b8cd8e4b87101fd6bf281c4bf11566765f48
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-07-10 14:48:50 +0200

    Add.

--- client/src/components/MainGrid.vue
+++ client/src/components/MainGrid.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
+import type { GridPosition } from '@/data/api';
 import Graph from './Graph.vue';
 import KeysList from './KeysList.vue';
+
+function getGridStyle(position: GridPosition) {
+  return {
+    'grid-area': `${position.rowStart} / ${position.columnStart} | ${position.rowEnd} / ${position.columnEnd}`
+  };
+}
 
 </script>
 
 <template>
 <main class="container-fluid">
-  <div class="grid">
+  <div class="main-grid">
     <KeysList />
     <Graph />
+    <div></div>
+    <div></div>
   </div>
 </main>
 </template>
+
+<style scoped>
+.main-grid {
+  display: grid;
+  grid-template-columns: repeat(20, 1fr);
+  grid-template-rows: repeat(20, 1fr);
+  grid-column-gap: var(--grid-spacing-horizontal);
+  grid-row-gap: var(--grid-spacing-vertical);
+}
+</style>
--- client/src/data/api.ts
+++ client/src/data/api.ts
@@ -6,6 +6,30 @@ export interface SearchParams {
   value?: string;
 }
 
+export enum ViewItemTypes {
+  List,
+  Keys,
+}
+
+export interface GridPosition {
+  rowStart: number | string;
+  columnStart: number | string;
+  rowEnd: number | string;
+  columnEnd: number | string;
+}
+
+export interface ViewItem extends GridPosition {
+  type: ViewItemTypes;
+}
+
+export interface View {
+  id: number;
+  name: string;
+  rows: number;
+  columns: number;
+  items: ViewItem[];
+}
+
 export async function getData<T>(type: GetDataType = 'entries', search: SearchParams = {}): Promise<T | null> {
     const params = new URLSearchParams(search as unknown as Record<string, string>);
 
