41bf674e5a87f174a8d2c1acc8e141484d3fec13
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-07-21 08:13:25 +0200

    Add.

---
+++ client/src/components/search/SearchAddFilter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineEmits(['add']);
+</script>
+
+<template>
+<a href="" class="secondary add" role="button" @click.prevent="$emit('add')">
+  <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <line x1="5" y1="12" x2="19" y2="12">'</line>
+  </svg>
+  Add filter
+</a>
+</template>
+
+<style scoped>
+a.add {
+  margin-bottom: var(--spacing);
+  margin-right: calc(var(--spacing) * .5);
+}
+a.add svg {
+  stroke: var(--secondary);
+  margin-right: 0.5rem;
+  border: 2px solid currentColor;
+  border-radius: 1rem;
+  background: currentColor;
+  vertical-align: unset;
+}
+</style>
--- client/src/components/search/SearchBar.vue
+++ client/src/components/search/SearchBar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import ModalDialog from '../ModalDialog.vue';
+import SearchAddFilter from './SearchAddFilter.vue';
 import SearchFilters from './SearchFilters.vue';
 
 interface Filter {
@@ -61,41 +62,9 @@ hello
     placeholder="Search"
     :value="props.modelValue"
     @input="$emit('update:modelValue', ($event?.target as HTMLInputElement)?.value)" />
-  <a href="" class="secondary add" role="button" @click.prevent="addFilter">
-    <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
-      <line x1="12" y1="5" x2="12" y2="19"></line>
-      <line x1="5" y1="12" x2="19" y2="12">'</line>
-    </svg>
-    Add filter
-  </a>
+  <SearchAddFilter @add="addFilter" />
   <SearchFilters
     :filters="filters"
     @select-filter="selectFilter" />
-  <!--<a
-    v-for="(filter, index) in filters"
-    v-bind:key="filter.id"
-    @click.prevent="selectFilter(index)"
-    href=""
-    role="button"
-    class="contrast outline filter">{{ filter.key }}: {{ filter.value }}</a>-->
 </div>
 </template>
-
-<style scoped>
-a.add {
-  margin-bottom: var(--spacing);
-  margin-right: calc(var(--spacing) * .5);
-}
-a.add svg {
-  stroke: var(--secondary);
-  margin-right: 0.5rem;
-  border: 2px solid currentColor;
-  border-radius: 1rem;
-  background: currentColor;
-  vertical-align: unset;
-}
-
-.filter {
-  margin-right: calc(var(--spacing) * .5);
-}
-</style>
