4d210850af0bbfd25c2d553e4f5da61c1f7f937a
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-10-06 22:22:22 +0200

    Add.

--- test/db.c
+++ test/db.c
@@ -8,7 +8,7 @@ void *test_db(void *arg) {
     ASSERT_CODE("NULL", WRD_ERROR, wrd_parse_json, NULL)
     ASSERT_CODE("NULL", WRD_ERROR, wrd_add_index, "", NULL)
     ASSERT_CODE("NULL", WRD_ERROR, wrd_db_close, NULL)
-    ASSERT_CODE("NULL", WRD_ERROR, wrd_load_columns, NULL)
+    // ASSERT_CODE("NULL", WRD_ERROR, wrd_load_columns, NULL)
     ASSERT("NULL", 0, wrd_last_insert_rowid, NULL)
 
     ASSERT_CODE("NULL", WRD_ERROR, wrd_add_entry, 0, NULL)
