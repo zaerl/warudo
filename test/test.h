91ccc1bacee964ffea9432a022791e714b859cdc
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2024-06-13 21:51:17 +0200

    Add.

--- test/test.h
+++ test/test.h
@@ -13,11 +13,30 @@ extern "C" {
 #endif
 
 #define MOCK_CONFIG warudo config;
-#define MOCK_FILE(NAME) FILE *NAME = tmpfile(); if(!NAME) ATT_ASSERT(0, 1, "tmpfile() failed");
+#define MOCK_CONFIG_S char *tmp_mock_file = NULL;
+
+#define MOCK_FILE(NAME) \
+    FILE *NAME = tmpfile(); \
+    if(!NAME) ATT_ASSERT(0, 1, "tmpfile() failed");
+
+#define FREE_MOCK_FILE_S \
+    if(tmp_mock_file) { \
+        remove(tmp_mock_file); \
+        free((void*)tmp_mock_file); \
+        tmp_mock_file = NULL; \
+    }
+#define MOCK_FILE_S(CONTENT) \
+    FREE_MOCK_FILE_S \
+    tmp_mock_file = wrd_mock_file(CONTENT); \
+    if(!tmp_mock_file) ATT_ASSERT(0, 1, "wrd_mock_file() failed")
 
 #define ASSERT_CODE(RESULT, EXPECTED, DESCRIPTION) wrd_assert_code(RESULT, EXPECTED, DESCRIPTION);
 
+// Assert that the wrd_code is equal to the expected value.
 void wrd_assert_code(int result, int expected, const char *description);
+
+// Generate a temporary file with the given content.
+char *wrd_mock_file(const char *content);
 
 #ifdef __cplusplus
 }
