491eb722c70614a8ffe8134e126d900d3712b1ce
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-04-05 11:43:13 +0200

    Remove obsolete files

--- client/src/views/NewDashboardView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import MainGrid from '@/components/MainGrid.vue';
-import SearchBar from '@/components/search/SearchBar.vue';
-</script>
-
-<template>
-<SearchBar />
-<MainGrid />
-</template>
